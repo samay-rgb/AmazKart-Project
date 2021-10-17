99b9e968864c35d7b4122c4a6e6b25910f9fca4c
samay-rgb <rahuldba99@gmail.com>
2021-10-17 20:45:52 +0530

    added validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,103 +1,92 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-//const { body, validationResult } = require("express-validator");
+const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
-const fetchuser = require("../middleware/fetchuser");
 const JWT_SECRET = "darthskywalker";
-//const fetchuser = require("../middleware/fetchuser");
-router.post("/createuser", async (req, res) => {
-  try {
-    // console.log(email);
-    useremail = `"${req.body.email}"`;
-
-    // if (data.length === 0) {
-    const salt = await bcrypt.genSalt(10);
-    //const email = String(req.body.email);
-    const secPas = await bcrypt.hash(req.body.password, salt);
-    db.query(
-      "INSERT INTO user(name,password,email,role) VALUES (?,?,?,?)",
-      [req.body.name, secPas, useremail, req.body.role],
-      (err, result) => {
-        if (err) {
-          res.send("User with given email exists");
-        } else {
-          console.log(result);
-          const data = {
-            user: {
-              id: useremail,
-            },
-          };
-
-          const authToken = jwt.sign(data, JWT_SECRET);
-          res.json({ authToken });
-        }
+const fetchuser = require("../middleware/fetchuser");
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 5 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 8,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
       }
-    );
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Some Error occured");
+      const salt = await bcrypt.genSalt(10);
+      const secPas = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        password: secPas,
+        email: req.body.email,
+      });
+      const data = {
+        id: user.id,
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
+    }
   }
-});
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    db.query(
-      "SELECT * FROM user WHERE email =?",
-      [email],
-      async (err, result) => {
-        if (err) {
-          return res.status(400).json({ error: "Wrong email id or password" });
-        } else {
-          //const rpass = result[0].password;
-          // console.log(rpass);
-          if (result.length === 0) {
-            return res
-              .status(400)
-              .json({ error: "Wrong email id or password" });
-          } else {
-            const passwordCompare = await bcrypt.compare(
-              password,
-              result[0].password
-            );
-            if (!passwordCompare) {
-              return res.status(400).json({ error: "Wrong password" });
-            }
+);
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-            const data = {
-              user: {
-                id: result[0].email,
-              },
-            };
-            console.log(data.user.id);
-            const authtoken = jwt.sign(data, JWT_SECRET);
-            res.json({ authtoken });
-          }
-        }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Wrong email id or password" });
       }
-    );
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "Wrong password" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
-});
+);
 router.post("/getuser", fetchuser, async (req, res) => {
   try {
-    const useremail = req.user.id;
-    // console.log(req.user.id);
-    db.query(
-      "SELECT name,email FROM user WHERE email = ?",
-      [useremail],
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(result);
-          res.send(result);
-        }
-      }
-    );
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
